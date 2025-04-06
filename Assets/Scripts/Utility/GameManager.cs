96db33af6088a753859b4664969fb125a55fb5bc
Amelka <126866176+amelia-kowalik@users.noreply.github.com>
2025-04-06 19:22:25 +0200

    Add gold and deaths manager

--- Assets/Scripts/Utility/GameManager.cs
+++ Assets/Scripts/Utility/GameManager.cs
@@ -1,14 +1,14 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static Action OnCatKidnapped;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
